488b93ab279b28474794cecd817d15cbf7b78880
aaron <aaschro@gmail.com>
2019-10-10 18:42:08 +0000

    Fixed typo in __init__.py

--- heartandsole/__init__.py
+++ heartandsole/__init__.py
@@ -1,5 +1,5 @@
 from heartandsole.activity import Activity
-from heartandsole.file_readers import FitActivity
+from heartandsole.filereaders import FitActivity
 
 #__version__ = '0.0.1'
 __all__ = ['Activity', 'FitActivity']
